9d9cb847a267adf143ccd95738b284a50abc74b6
Pinku Rathour <pinkurathour2016@gmail.com>
2025-09-05 16:40:15 +0530

    cretae a new projects

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-center font-semibold text-2xl">
+          Learn Redux Toolkit
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
